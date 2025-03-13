5fa4b3015f6b34dcabe4b514def9421c80ce7ded
Zimmery <lukas.r.m.zimmermann@gmail.com>
2025-03-13 08:54:09 +0100

    switch from cubic to linear;

--- deepdrr/projector/project_kernel.cu
+++ deepdrr/projector/project_kernel.cu
@@ -74,7 +74,7 @@
 #define LOAG_SEGS_FOR_VOL_MAT(vol_id, mat_id)                                  \
   do {                                                                         \
     seg_at_alpha[vol_id][mat_id] = round(                                      \
-        cubicTex3D<float, float>(SEG_TEX(vol_id, mat_id), make_float3(px[vol_id], py[vol_id], pz[vol_id])));  \  
+        tex3D<float>(SEG_TEX(vol_id, mat_id), px[vol_id], py[vol_id], pz[vol_id]));  \  
   } while (0)
 
 // TODO: rather than having num vols lines for each macro, define the macro once
